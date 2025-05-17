50c4f5a16c4bd7f5eded91692e30070d4b7c1b7a
Esdras Reis <reisesdras36@gmail.com>
2025-05-17 13:36:02 -0300

    Delete atividade4 directory

--- atividade4/src/br/edu/fatecpg/atividade4/model/Pagamento.java
+++
@@ -1,9 +0,0 @@
-package br.edu.fatecpg.atividade4.model;
-
-public abstract class Pagamento {
-	
-	public void ProcessarPagamento() {
-		System.out.println("Processando pagamento generico");
-	}
-
-}
