21027878bc27db4c0d79f1930f3754a8da2eb65c
Esdras Reis <reisesdras36@gmail.com>
2025-05-17 13:51:41 -0300

    Add files via upload

---
+++ Sobrecargar, Sobrescrita e hierarquia/sobrescrita de metodos/Pagamento/src/br/edu/fatecpg/atividade4/model/Pagamento.java
@@ -0,0 +1,9 @@
+package br.edu.fatecpg.atividade4.model;
+
+public abstract class Pagamento {
+	
+	public void ProcessarPagamento() {
+		System.out.println("Processando pagamento generico");
+	}
+
+}
---
+++ Sobrecargar, Sobrescrita e hierarquia/sobrescrita de metodos/Veiculo/src/br/edu/fatecpg/model/Carro.java
@@ -0,0 +1,9 @@
+package br.edu.fatecpg.model;
+
+public class Carro extends Veiculo{
+	 @Override
+	public void mover() {
+		System.out.println("O carro esta dirgindo!!");
+	}
+
+}
